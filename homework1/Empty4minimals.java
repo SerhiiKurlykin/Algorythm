a22a3e3d878e66eb81106dd38e3fd0db636b3a65
Serhii Kurlykin <kurlykin83@gmail.com>
2024-11-25 14:20:35 +0100

    Update Empty4minimals.java
    correct

--- homework1/Empty4minimals.java
+++ homework1/Empty4minimals.java
@@ -3,30 +3,30 @@ package org.example.homework1;
 public class Empty4minimals {
 
     public static void main(String[] args) {
-        int[] nums = {23, 4, 53, 62, 345, 5, 0};
+        int[] nums = {0, -23, 4, 53, 4, 345, 5, 0, 5, 7};
         System.out.println(findMin(nums));
     }
 
     private static int findMin(int[] nums) {
-        int min1 = nums[0];
-        int min2 = nums[0];
-        int min3 = nums[0];
-        int min4 = nums[0];
+        int min1 = Integer.MAX_VALUE;;
+        int min2 = Integer.MAX_VALUE;;
+        int min3 = Integer.MAX_VALUE;;
+        int min4 = Integer.MAX_VALUE;;
 
         for (int num : nums) {
-            if (num < min1) {
+            if (num <= min1) {
                 min4 = min3;
                 min3 = min2;
                 min2 = min1;
                 min1 = num;
-            } else if (num < min2 && num != min1) {
+            } else if (num <= min2 && num != min1) {
                 min4 = min3;
                 min3 = min2;
                 min2 = num;
-            } else if (num < min3 && num != min1 && num != min2) {
+            } else if (num <= min3 && num != min1 && num != min2) {
                 min4 = min3;
                 min3 = num;
-            } else if (num < min4 && num != min1 && num != min2 && num != min3) {
+            } else if (num <= min4 && num != min1 && num != min2 && num != min3) {
                 min4 = num;
             }
         }
